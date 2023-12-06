1767996109aa48dc0fb321acdf7f443b5d08f8c4
Cesar <csr2453@outlook.es>
2023-12-06 10:14:14 -0400

    Variables GLOBALES

--- modulos/globales.js
+++ modulos/globales.js
@@ -1,1 +1,21 @@
-console.log(process);
+/**
+ * Son un monton de modulos, que vienen incluidos como modulos globales, ejm, conole.log,setInterval, setTimeout, etc.
+ */
+
+//global=> es el objeto donde se encuentran todos los módulos globales guardados
+//Ejemplo
+let i = 0;
+let intervalo = setInterval(() => {
+    console.log('Hello world');
+    if (i===3) {
+        clearInterval(intervalo);
+    }
+    i++;
+}, 1000);
+setImmediate(()=>
+    console.log('Rapido...') 
+);
+console.log(__dirname);//Obtiene el nombre del directorio donde se encuentra el archivo
+console.log(__filename);//Obtiene el nombre del archivo mas la ruta
+global.miVariable = 'Mi valor';//Creación de variables globales
+console.log(global);
