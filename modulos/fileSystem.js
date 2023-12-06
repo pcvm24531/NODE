9bab9eeaa3156be140d39676d7937ac481e694dc
Cesar <csr2453@outlook.es>
2023-12-06 14:54:41 -0400

    SISTEMA DE ARCHIVOS, Ejemplos sf

--- modulos/fileSystem.js
+++ modulos/fileSystem.js
@@ -1,25 +1,43 @@
 /**
  * File System: FS nos permite acceder a archivos del sistema.
- * 
+ * Podemos acceder, leerlos, sobre escribirlos, cambiarles el nombre.
  */
 const fs = require('fs');
+const path = require('path');
 
 function reading(path, cb) {
     fs.readFile(path, (err, data)=>{
         console.log(data.toString());
     } );
 }
+//Remplaza el contenido
 function writing(path, content, cb){
     fs.writeFile(path, content, (err)=>{
         if(err){
-            console.log('Nothing write...');
+            console.error('Nothing write...', err);
         }else{
-            console.log('Writing correctly..');
+            console.log('File Writing correctly..');
         }
     });
 }
 
-//reading( __dirname + '/archivo.txt' );
-writing( __dirname+'/archivo.txt', 'Son contenido nuevo', console.log(12) );
+function deleting(path, cb) {
+    fs.unlink(path, ()=>{
+        console.log('File deleted');
+        cb();
+    });
+}
 
+function main() {
+    setTimeout(
+        ()=>{
+            reading( __dirname + '/archivo.txt' );
+            writing( __dirname+'/archivo1.txt', 'Soy contenido nuevo', console.log );
+            deleting( __dirname+'/archivo1.txt', console.log ); 
+        }, 1000
+    );
+}
+
+main();
 reading( __dirname + '/archivo.txt' );
+
