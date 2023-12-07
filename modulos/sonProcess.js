9c3ced3803e2800e1d921210f98c581ff6ebff7d
Cesar <csr2453@outlook.es>
2023-12-07 12:31:58 -0400

    Ejemplos hijos process

--- modulos/sonProcess.js
+++ modulos/sonProcess.js
@@ -5,19 +5,25 @@
 const { exec } = require('child_process');
 //o //const  exec = require('child_process').exec;
 
-//exec('ls -la', (err, stdout, sterr)=>{//Comando linux
-//exec('dir', (err, stdout, sterr)=>{//Comando Windows
-/*exec('node modulos/console.js', (err, stdout, sterr)=>{//
+//exec('ls -la', (err, stdout, sterr)=>{//Comando linux, para listar archivos y directorios
+//exec('dir', (err, stdout, sterr)=>{//Comando Windows, para listar archivos y directorios
+/*exec('node modulos/console.js', (err, stout, sterr)=>{//Comando que ejecuta un proceso node, que ejecuta otro proceso
     if(err){
         console.error(err);
         return false;
     }
-    console.log(stdout);
+    console.log(stout);
 } );*/
 
 
 //Ejemplo para ejecutar un proceso nuevo
+//spawn nos permite invocar un proceso nuevo de nodejs
 const { spawn } = require('child_process');
 
 let proceso = spawn('ls', ['-la']);
 console.log(proceso.pid);
+console.log(proceso.connected);
+
+proceso.stdout.on('data',(dato)=>{
+    console.log(dato.toString());
+});
