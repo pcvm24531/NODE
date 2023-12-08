f58744fdc4e35d6cf807b3cf973cbf8c96c5a0e5
Cesar <csr2453@outlook.es>
2023-12-08 16:50:25 -0400

    PROCESS, ejemplos para controlar cuando termina un proceso, antes de terminar, capturar errores no controlados

---
+++ modulos/process.js
@@ -0,0 +1,27 @@
+/**
+ * Procesos, Aprenderemos a acceder a nuestro proceso, escuchar señales y hacer cosas con ellos
+ */
+
+//const p = require('process');//
+//Tambien se puede acceder de esta manera
+
+//Detectar cuando el programa esta por terminar
+//Cuando se ejecuta el exit, eso quiere decir que ya nos hemos desconectado completamente del eventloop
+process.on( 'exit', ()=>{
+    console.log('EL proceso acabó');
+} );
+
+//Detectar antes de que el proceso termine
+process.on( 'beforeExit', ()=>{
+    console.log('El proceso va a terminar');
+} );
+
+//Detectar una excepcion que no ha sido capturado
+//Evitar la falla de errores no controlados
+process.on('uncaughtException',(err,origen)=>{
+    console.error('Olvidamos capturar un error');
+    console.error(err.message);
+});
+
+funciones();
+console.log('Llegamos? o ya no?');
