df4f8901e1efcf51303b065768a9e438a19dc3cc
Cesar <csr2453@outlook.es>
2023-12-15 16:42:32 -0400

    Cambio en stream

--- memoria/stream.js
+++ memoria/stream.js
@@ -11,3 +11,11 @@ readableStream.setEncoding('utf-8');//Codificamos con UTF8 para no usar el toStr
 readableStream.on('data', (chunk)=>{
     console.log(chunk);
 } );
+
+
+/**
+ * Ejemplo stream de escritura
+ */
+process.stdout.write('Hola');
+process.stdout.write('que');
+process.stdout.write('tal');
