e0e732e50c59306c246fb4a963dd9f6c473f69f9
Cesar <csr2453@outlook.es>
2024-02-01 12:27:04 -0400

    Importar y exportar por modulos

---
+++ nodejs/cjs/index.mjs
@@ -0,0 +1,6 @@
+// .js -> Por defecto usa CommonJS
+// .mjs -> para utilizar ES Modules
+// .cjs -> para utilizar CommonJS
+
+import { suma } from "./suma.mjs";
+console.log( 'sumaMjs: ',suma(3,2) );
