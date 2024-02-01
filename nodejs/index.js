2ade08a65359eaa6ddc025e4f47dd986ba1e87bb
Cesar <csr2453@outlook.es>
2024-02-01 12:12:42 -0400

    Exportar e importar con CommonJS

---
+++ nodejs/index.js
@@ -0,0 +1,4 @@
+//CommonJS, modo de importar un modulo
+const {suma} = require('./suma');
+
+console.log( 'suma: ', suma(1, 2) );
---
+++ nodejs/suma.js
@@ -0,0 +1,9 @@
+
+function suma(nr1, nro2) {
+    return nr1 + nro2;
+}
+
+//CommonJS, modo de exportar un módulo
+module.exports = {
+    suma
+}
