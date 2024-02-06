e8009d3b71a2a41a5087f524f67948108bd7587e
Cesar <csr2453@outlook.es>
2024-02-06 17:05:02 -0400

    Ejemplo básico de http

---
+++ http/index.js
@@ -0,0 +1,16 @@
+const http = require('node:http');
+const hostname = "127.0.0.1";
+const port = 3000;
+
+//Creamos un servidor
+const server = http.createServer(
+    (req, res)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("Hello, Pablo");
+    }
+);
+server.listen( port, hostname, ()=>{
+        console.log(`Ejecutando en server ${hostname} y puerto ${port}`);
+    }
+);
