522e21841961f520ca9c9be2ce5ac6ced432dbd4
Cesar <csr2453@outlook.es>
2024-02-07 10:03:28 -0400

    Modulo assert legacy model

--- assert/index.js
+++ assert/index.js
@@ -2,4 +2,9 @@ const assert = require('node:assert/strict');
 
 //assert.deepEqual( [[[1,2,3]],4,5 ], [[[1,2,3]],4,5 ] );
 
-console.log(  assert.deepEqual( [[[1,2,3]],4,5 ], [[[1,2,3]],4,5 ] ) );
+try {
+    assert.strictEqual([[[1, 2, 3]], 4, 5], [[[1, 2, '3']], 4, 5]);
+} catch (error) {
+    assert(error instanceof assert.AssertionError);
+    assert.strictEqual( error.message, message );
+}
