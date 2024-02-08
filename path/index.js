f3f7c4bf9bda50b3fb691c4cc51a48a8d64f9e66
Cesar <csr2453@outlook.es>
2024-02-08 15:38:39 -0400

    Path, ejemplo con filename, extension, etc

---
+++ path/index.js
@@ -0,0 +1,17 @@
+const path = require('node:path');
+
+//Barra separadora de carpetas según SO
+console.log(path.sep);//Devuelve el / o \ dependiendo del SO
+
+//Unir rutas con path.join
+const filePath = path.join('content','subfolder','test.txt');
+console.log('This is filePaht: ', filePath);
+
+const base = path.basename('../fileSystem/archivo2.txt');
+console.log('This is base path: ',base);
+//Obtenemos solo el nombre sin extension
+const fileName = path.basename('../fileSystem/archivo2.txt', '.txt');
+console.log('Only name: ', fileName);
+//Obtenemos solo la extension del archivo
+const extension = path.extname('../fileSystem/archivo2.txt');
+console.log('The file extension: ',extension);
