089208938ad7faf17543076da77ee75458c8f117
Cesar <csr2453@outlook.es>
2024-02-16 15:11:02 -0400

    salvando ejemplos

---
+++ npm/index.js
@@ -0,0 +1,26 @@
+//NPM = Node Package manager
+//Es el registro de paquetes más grande de codigo abierto
+//
+//Inicializamos un proyecto de cero
+/**
+ * COMANDOS pRA CREAR un PROYECTO
+ * 1.- npm init ->Crea un pacckage.json, el cual contiene la informacion del autor, dependencias, scripts, etc.
+ * 2.- npm init
+ *      -package name: nombreProyecto
+ *      -version: (1.0.0)
+ *      -description: El mejor curso de node
+ *      -entry point: index.js//Es el punto de entrada de nuestro proyecto, es el archivo que al ejecutarlo levanta todo el proyecto
+ *      -test command: 
+ *      -git repository: Colocamos el url del repositorio
+ *      -keywords: curso, node, nodejs
+ *      -author: Pablo vargas
+ *      -license: MIT
+ * 
+ *  --Instalar picoscolors
+ *  npm install picocolors
+ *  npm install loadsh
+ *  npm install react
+ *  npm uninstall react
+ *  npm unistall lodash
+ *  npm install standard -D ->Para formatear o estandarizar el código, el -D indica que es una dependencia de desarrollo
+ */
