3bb7c98a33011c3f4bd1f63907debd44ed95d05c
Cesar <csr2453@outlook.es>
2024-02-16 15:06:40 -0400

    Funcion para obtener un puerto disponible

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const http = require('node:http');
+const {finAvailablePort} = require('../port/index');
+
+const desiredPort = process.env.PORT ?? 3000 ;
 
 const server = http.createServer(
     ( req, res )=>{
@@ -7,6 +10,12 @@ const server = http.createServer(
     }
 );
 
-server.listen( 0, ()=>{
+/*server.listen( 0, ()=>{
     console.log( `Server listening on port http://localhost:${server.address().port}` );
-} );
+} );*/
+
+finAvailablePort(desiredPort).then( port =>{
+    server.listen( port, ()=> {
+        console.log( `Server listening on port http://localhost:${server.address().port}` );
+    })
+});
